b87f1e05e5e58a9d6ec9e34e200ce92e6a7daa90
Ernys Madariaga <ernysmadariaga@gmail.com>
2023-06-08 21:06:51 -0400

    refactored code and made CreateApartment method generic

---
+++ Software2KnowledgeCheck1/BuildingRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    public class BuildingRepo
+    {
+        public List<Building> Buildings { get; } = new List<Building>();
+
+        //public void CreateApartment(Apartment apartment)
+        public void CreateBuilding<T>(Building building) where T : Building
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var constructionRepo = new ConstructionRepo();
+
+            var buildingWasMade = constructionRepo.ConstructBuilding<T>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                Buildings.Add(building);
+            }
+        }
+    }
+}
---
+++ Software2KnowledgeCheck1/ConstructionRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    public class ConstructionRepo
+    {
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
